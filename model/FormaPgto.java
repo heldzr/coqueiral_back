53dce83155d1c99d9e2ee800b100e8f2f7efbeb3
Clara-oliveiraa <claraoliveira.8351@gmail.com>
2024-04-27 00:05:58 -0300

    Adicionando classe: forma de pagamento

---
+++ model/FormaPgto.java
@@ -0,0 +1,24 @@
+public class FormaPgto{
+    private string idFormaPgto;
+    private string descricao;
+    private string tipoFormaPgto;
+
+    public string getIdFormaPgto(){
+        return idFormaPgto;
+    }
+    public void setIdFormaPgto(string idFormaPgto){
+        this.idFormaPgto = idFormaPgto;
+    }
+    public string getDescricao(){
+        return descricao;
+    }
+    public void setDescricao(string descricao){
+        this.descricao = descricao;
+    }
+    public string getTipoFormaPgto(){
+        return tipoFormaPgto;
+    }
+    public void setTipoFormaPgto(string tipoFormaPgto){
+        this.tipoFormaPgto = tipoFormaPgto;
+    }
+}
