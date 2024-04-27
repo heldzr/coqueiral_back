d0fc96746e844f0e5f64d3961048a041c9c0c2e4
Acacio.01 <acacio.vini01@gmail.com>
2024-04-27 00:24:44 -0300

    adicionando classe produtos

---
+++ model/Produto.java
@@ -0,0 +1,40 @@
+public class Produto {
+   
+    private String idProduto;
+    private String nomeProduto;
+    private String descricaoProduto;
+    private float precoProduto;
+    private int quantDisponivel;
+    
+    public String getIdProduto() {
+        return idProduto;
+    }
+    public void setIdProduto(String idProduto) {
+        this.idProduto = idProduto;
+    }
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+    public float getPrecoProduto() {
+        return precoProduto;
+    }
+    public void setPrecoProduto(float precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+    public int getQuantDisponivel() {
+        return quantDisponivel;
+    }
+    public void setQuantDisponivel(int quantDisponivel) {
+        this.quantDisponivel = quantDisponivel;
+    }
+
+}
